367a99cce68693cf202847ed8084aae285942b3b
Thiago Caiado <thiago.caiado@hotmail.com>
2019-01-12 15:51:37 -0200

    Add files via upload

--- src/components/BookInfo.js
+++ src/components/BookInfo.js
@@ -3,15 +3,35 @@ import '../styles/App.css';
 
 export default class BookInfo extends React.Component {
     render() {
+        
         const { book, fromDetails } = this.props;
+        console.log(book)
         return (
             <div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors}</div>
+                <div className={book.title
+                    ? 'book-title'
+                    : 'book-title-found'}>
+                    {book.title
+                        ? book.title
+                        : 'no title found...'}
+                </div>
+                <div className={book.authors
+                    ? 'book-authors'
+                    : 'book-authors-found'}>
+                    {book.authors ? book.authors : 'no author found...'}</div>
                 {fromDetails && (
                     <div>
-                    <div className="book-date">{`Release Date: ${book.publishedDate}`}</div>
-                    <div className="book-description">{book.description}</div>
+                        <div className={book.publishedDate
+                            ? 'book-publishedDate'
+                            : 'book-publishedDate-found'}>
+                        {book.publishedDate
+                                ? `Release Date: ${book.publishedDate}`
+                                : 'no release date found...'}
+                        </div>
+                        <div className={book.description
+                            ? 'book-description'
+                            : 'book-description-found'}>
+                            {book.description ? book.description : 'no description found...'}</div>
                     </div>
                 )}
             </div>
